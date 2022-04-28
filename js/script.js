45ac43d319aab67ebb9ddd5cda7834da144e5e15
katy01101011 <katy.91560@gmail.com>
2022-04-28 07:48:52 +0200

    add: result in html; manca: controllo sul prompt

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
-// Visualizzare in pagina 5 numeri casuali.
-// Da lì parte un timer di 30 secondi.
-// Dopo 30 secondi l'utente deve inserire, uno alla volta,
-// i numeri che ha visto precedentemente, tramite il prompt().
-// Dopo che sono stati inseriti i 5 numeri,
-// il software dice quanti e quali dei numeri da indovinare
-// sono stati individuati.
-
 // GLOBAL STATE OF GAME
-const arrayRandomNumbers = [];
 const difficult = 5;
-let showNumbers = document.querySelector('h1');
-
-
+const arrayRandomNumbers = [];
+console.log(arrayRandomNumbers);
+let timeLeft = 10;
+const userNumbers = [];
+const gotNumbers = [];
+console.log(gotNumbers);
+console.log(userNumbers.length);
+let gotNumber;
 
 // Creo un array con i numeri da visualizzare
 let i = 0;
@@ -24,6 +20,7 @@ while (i < difficult) {
 }
 
 // Stampo i numeri in HTML
+let showNumbers = document.querySelector('h1');
 for (i = 0; i < difficult; i++) {
     let htmlNumber = arrayRandomNumbers[i];
     showNumbers.innerHTML += `<span> ${htmlNumber} </span>`
@@ -32,36 +29,42 @@ for (i = 0; i < difficult; i++) {
 
 // Creo un timer che parte da 10 secondi e arriva a 0
 let timerContainer = document.getElementById('timer');
-let timeLeft = 3;
 timerContainer.innerHTML = timeLeft;
-const userNumbers = [];
+console.log(userNumbers);
 
+// Chiedo all'utente i 5 numeri che si ricorda
 const timerCount = setInterval(function() {
         timeLeft--;
         timerContainer.innerHTML = timeLeft;
-        if (timeLeft === 0) {
-            document.querySelector('div').classList.add('hidden')
-        }
-        if (timeLeft < 0) {
-            clearInterval(timerCount)
-            for (let i = 0; i < difficult; i++) {
-                const userNumber = parseInt(prompt('Quale numero c\'era nell\'elenco?'));
-
-                if (!userNumbers.includes(userNumber)) {
-                    userNumbers.push(userNumber);
-                    console.log(userNumbers);
-                } else {
-                    alert('Hai già inserito questo numero!')
-                }
-
-            }
+        if (timeLeft <= 0) {
+            document.getElementById('start-game').classList.add('hidden')
         }
         
+        // Confronto i numeri che ha fornito l'utente con i numeri generati dal computer
+        let sum = 0
+        if (timeLeft < 0) {
+            clearInterval(timerCount)
+            for (i = 0; i < difficult; i++) {
+
+                
+                const userNumber = parseInt(prompt('Quale numero c\'era nell\'elenco?'));
+                userNumbers.push(userNumber);
+                if (arrayRandomNumbers.includes(userNumber)) {
+                    gotNumbers.push(userNumber)
+                    // Inserisco il risultato in HTML
+                    document.getElementById('got-numbers').innerHTML += `<span>${userNumber}  </span>`;
+                    sum += 1;
+                    document.getElementById('game-result').innerHTML = sum;
+                }
+            }
+        }
+
+        // Stampo il risultato
+        if (userNumbers.length === 5) {    
+            document.getElementById('end-game').classList.replace('hidden', 'show')
+        }
     }, 1000
 )
-
-
-
 
 // FUNCTIONS
 
@@ -73,15 +76,3 @@ function randomIntNumber() {
     const number = Math.floor(Math.random() * 100) + 1;
     return number;
 }
-
-
-
-
-// // Lascio i numeri sullo schermo per 3 secondi, poi scompaiono
-// setTimeout(()=> {
-//     showNumbers.classList.add('hidden');
-
-//     setTimeout(() => {
-//         getUserNumbers();
-//     }, 100);
-// }, 3000)
